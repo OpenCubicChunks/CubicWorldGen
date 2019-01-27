aa50337265bece314a3a9cfb806fafb8428e30c6
Foghrye4 <foghrye4@gmail.com>
2019-01-27 17:32:42 +0300

    Fix generator settings not saved on creating new world

--- src/main/java/io/github/opencubicchunks/cubicchunks/cubicgen/asm/mixin/common/MixinSaveHandler.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/cubicgen/asm/mixin/common/MixinSaveHandler.java
@@ -23,13 +23,17 @@
  */
 package io.github.opencubicchunks.cubicchunks.cubicgen.asm.mixin.common;
 
+import javax.annotation.Nullable;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import io.github.opencubicchunks.cubicchunks.cubicgen.common.world.storage.IWorldInfoAccess;
 import io.github.opencubicchunks.cubicchunks.cubicgen.customcubic.CustomGeneratorSettings;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.storage.ISaveHandler;
 import net.minecraft.world.storage.SaveHandler;
 import net.minecraft.world.storage.WorldInfo;
@@ -46,5 +50,12 @@ public class MixinSaveHandler {
             return;
         IWorldInfoAccess worldInfo = (IWorldInfoAccess) cir.getReturnValue();
         worldInfo.setGeneratorOptions(generatorOptions);
+    }
+    
+    @Inject(method = "saveWorldInfoWithPlayer", at = @At("RETURN"))
+    public void onSavingWorldInfoWithPlayer(WorldInfo worldInformation, @Nullable NBTTagCompound tagCompound,
+            CallbackInfo ci) {
+        if (!CustomGeneratorSettings.getPresetFile((ISaveHandler) (Object) this).exists())
+            CustomGeneratorSettings.saveToFile((ISaveHandler) (Object) this, worldInformation.getGeneratorOptions());
     }
 }
