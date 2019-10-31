805dc5f8727f67485474e971abd7317a3a39bd73
Barteks2x <Barteks2x@gmail.com>
2019-10-31 15:00:08 +0100

    Update MixinGradle

--- CubicWorldGen/build.gradle.kts
+++ CubicWorldGen/build.gradle.kts
@@ -22,7 +22,7 @@ buildscript {
         }
     }
     dependencies {
-        classpath("org.spongepowered:mixingradle:0.5-SNAPSHOT")
+        classpath("org.spongepowered:mixingradle:0.6-SNAPSHOT")
         classpath("com.github.jengelman.gradle.plugins:shadow:2.0.4")
         classpath("gradle.plugin.nl.javadude.gradle.plugins:license-gradle-plugin:0.14.0")
         classpath("net.minecraftforge.gradle:ForgeGradle:2.3-SNAPSHOT")
