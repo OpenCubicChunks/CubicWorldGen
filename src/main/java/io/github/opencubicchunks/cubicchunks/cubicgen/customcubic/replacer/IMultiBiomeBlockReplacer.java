da13cc80fe7d135ccc117d87b63a8feff069655b
DaPorkchop_ <daporkchop@daporkchop.net>
2023-08-26 16:45:12 +0200

    fix copyright

--- src/main/java/io/github/opencubicchunks/cubicchunks/cubicgen/customcubic/replacer/IMultiBiomeBlockReplacer.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/cubicgen/customcubic/replacer/IMultiBiomeBlockReplacer.java
@@ -1,3 +1,26 @@
+/*
+ *  This file is part of Cubic World Generation, licensed under the MIT License (MIT).
+ *
+ *  Copyright (c) 2015-2020 contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
 package io.github.opencubicchunks.cubicchunks.cubicgen.customcubic.replacer;
 
 import mcp.MethodsReturnNonnullByDefault;
@@ -9,6 +32,7 @@ import javax.annotation.ParametersAreNonnullByDefault;
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public abstract class IMultiBiomeBlockReplacer {
+
     /**
      * Replaces the given block with another block based on given location, density gradient and density value. Biome
      * block replacers can be chained (output if one replacer used as input to another replacer)
